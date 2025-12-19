558037555a7f184d8c3e089bb59edf455a4e29d4
ShiroiTree <dammond_max@outlook.com>
2025-12-19 20:40:59 +0800

    chore: modified class define

--- app/src/main/java/org/LunaTelecom/model/UserAccount.java
+++ app/src/main/java/org/LunaTelecom/model/UserAccount.java
@@ -1,46 +1,14 @@
 package org.LunaTelecom.model;
 
 import java.time.LocalDateTime;
+ 
+import lombok.Data;
 
-import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
-
+@Data
 public class UserAccount {
     private long id;
     private String idCard;
     private String name;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    public UserAccount() {
-    }
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getIdCard() {
-        return idCard;
-    }
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-    public LocalDateTime getupdatedAt() {
-        return updatedAt;
-    }
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
